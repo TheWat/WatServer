8becfdc6e59d65fe0847b0bad59bd8c28ae0f0b6
jwesly <wjabezkingsly@gmail.com>
2016-02-21 09:26:29 -0500

    UDP First Try

---
+++ udp.js
@@ -0,0 +1,49 @@
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/pdata");
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log("Mongo Connected");
+});
+
+var dSchema = mongoose.Schema({
+	time: Number
+	,power: Number
+	,current: Number
+	,voltage: Number
+});
+
+var snapshot = mongoose.model('Snapshot',dSchema);
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+    //TODO
+    /****************************************************************************
+	Parse the message [timestamp, power(Watts), current(amps), voltage(volts)]
+
+	Store in mongo database
+
+    ****************************************************************************/
+    message = message.split(" ");
+    //TODO- store numbers in binary
+    var row  = new Snapshot({
+    	time:message[0]
+    	,power:message[1]
+    	,current:message[2]
+    	,voltage:message[3]
+    });
+    row.save(function(err,row){
+    	if(err) return console.error(err);
+    })
+
+
+});
+
+server.bind(PORT, HOST);
