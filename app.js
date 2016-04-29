5b95dc96101a8ac0704dd2255411e70476d9d4f5
jwesly <wjabezkingsly@gmail.com>
2016-04-28 22:47:21 -0400

    Graphs are cool

--- app.js
+++ app.js
@@ -29,12 +29,29 @@ app.get("/",function(req,res){
 	res.sendFile("basicUI.html",{root: __dirname});
 	console.log("rendering");
 });
+app.get("/graph",function(req,res){
+	res.sendFile("graphUI.html",{root: __dirname});
+	console.log("rendering");
+});
 app.get("/ajax/:watid",function(req,res){
 	var wat = req.params.watid;
 	Snapshot.findOne({'id':wat},'power current voltage',{sort:{serverTime:-1}},function(err,row){
 		if (err) return;//not good practice
 		if(row)//maybe this will stop the crashes
 			res.send(row.power + " " + row.current + " " + row.voltage);
+	})
+});
+app.get("/grajax/:watid",function(req,res){
+	var wat = req.params.watid;
+	Snapshot.
+		find({'id': wat}).
+		limit(20).
+		sort({serverTime:-1}).
+		select({power:1}).
+		exec(function(err,data){
+			if (err) return;//not good practice
+			console.log(data);
+		});
 	})
 });
 
